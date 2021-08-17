6ef0a1a47362cca18202e24cac9af325787f3033
Knut Berg <knut.berg@nord.no>
2021-08-17 21:33:25 +0200

    .

---
+++ user-info.el
@@ -0,0 +1,5 @@
+(setq user-full-name         "Knut Berg"
+      user-mail-address      "headpoke@gmail.com"
+      calendar-latitude      67.289
+      calendar-longitude     14.560
+      calendar-location-name "Bodø, Norway")
